5d743f8a878ec7424e89f52b8c0453f360589862
Elmin Omanovic <105084685+elmin387@users.noreply.github.com>
2022-06-22 12:54:51 +0200

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,43 @@
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+// Mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    
+    if(ent.isIntersecting===false){
+
+    document.body.classList.add("sticky");
+    }
+
+    if(ent.isIntersecting===true){
+
+      document.body.classList.remove("sticky");
+      }
+  },
+    
+
+  {
+  root: null,
+  threshold: 0,
+  rootMargin:"-80px",
+}
+);
+obs.observe(sectionHeroEl);
